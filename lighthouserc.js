b51c91e7f119346625711c90f745d110a2aec2dc
Zach Rutman <zarutman+git@gmail.com>
2024-06-02 18:41:12 -0700

    fix: workflow

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       url: [process.env["DEPLOY_URL"]+'?nomotion'],
-      startServerCommand: 'echo started',
+      startServerCommand: 'echo ready',
     },
     upload: {
       target: 'temporary-public-storage',
